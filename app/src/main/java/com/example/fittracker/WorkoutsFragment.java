219eae8e2275d7fb45d8affcefcaeab476e073a5
danielg02 <danielgobalakrishnan@gmail.com>
2020-05-25 20:01:20 -0400

    Started RecyclerView

--- app/src/main/java/com/example/fittracker/WorkoutsFragment.java
+++ app/src/main/java/com/example/fittracker/WorkoutsFragment.java
@@ -1,5 +1,6 @@
 package com.example.fittracker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,18 +9,45 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class WorkoutsFragment extends Fragment {
+    private ArrayList<String> mWorkoutNames = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workouts, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_workouts, container, false);
+        // 1. get a reference to recyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.rvWorkouts);
+
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // this is data for recycler view
+        mWorkoutNames.add("Hello");
+        mWorkoutNames.add("Hello2");
+        mWorkoutNames.add("Hello3");
+
+        // 3. create an adapter
+        RecyclerViewAdapter mAdapter = new RecyclerViewAdapter(mWorkoutNames);
+        // 4. set adapter
+        recyclerView.setAdapter(mAdapter);
+        // 5. set item animator to DefaultAnimator
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+
 }

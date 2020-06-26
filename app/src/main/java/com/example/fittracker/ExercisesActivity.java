a073353af093129b94938d3b7cc605c4a11c29e7
danielg02 <danielgobalakrishnan@gmail.com>
2020-06-25 20:46:43 -0400

    Finished Delete Functions

--- app/src/main/java/com/example/fittracker/ExercisesActivity.java
+++ app/src/main/java/com/example/fittracker/ExercisesActivity.java
@@ -5,6 +5,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,13 +31,74 @@ public class ExercisesActivity extends Activity {
         List<String> reps = dbHelper.getReps(workoutID);
         List<String> weight = dbHelper.getWeight(workoutID);
 
-        for (int i = 0; i < dbHelper.getExercise(workoutID).size(); i++){
+        for (int i = 0; i < dbHelper.getExercise(workoutID).size(); i++) {
             exercises.add(new ExerciseItem(exerciseNames.get(i), sets.get(i), reps.get(i), weight.get(i)));
         }
 
         RecyclerView rv = findViewById(R.id.exercise_recycler_view);
         rv.setLayoutManager(new LinearLayoutManager(this));  //Positions the items
-        rv.setAdapter(new com.example.fittracker.ExerciseRVAdapter(exercises));
+        rv.setAdapter(new ExerciseRVAdapter(exercises));
+    }
+
+
+    private class ExerciseRVAdapter extends RecyclerView.Adapter<ExerciseRVAdapter.ExerciseViewHolder> {
+        List<ExerciseItem> exercises;
+
+        public ExerciseRVAdapter(List<ExerciseItem> exercises) {
+            this.exercises = exercises;
+        }
+
+        public class ExerciseViewHolder extends RecyclerView.ViewHolder {
+            TextView exerciseName;
+            TextView numOfSets;
+            TextView numOfReps;
+            TextView weight;
+            Button deleteExercise;
+
+            ExerciseViewHolder(View itemView) {
+                super(itemView);
+                exerciseName = itemView.findViewById(R.id.exercise);
+                numOfSets = itemView.findViewById(R.id.sets);
+                numOfReps = itemView.findViewById(R.id.reps);
+                weight = itemView.findViewById(R.id.weight);
+                deleteExercise = itemView.findViewById(R.id.delete_exercise_button);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return exercises.size();
+        }
+
+        @Override
+        public ExerciseViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.exercise_card, viewGroup, false);
+            ExerciseViewHolder exerciseViewHolder = new ExerciseViewHolder(view);
+            return exerciseViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(final ExerciseViewHolder evh, int i) {
+            evh.exerciseName.setText(exercises.get(i).getName());
+            evh.numOfSets.setText(exercises.get(i).getSets());
+            evh.numOfReps.setText(exercises.get(i).getReps());
+            evh.weight.setText(exercises.get(i).getWeight());
+
+            evh.deleteExercise.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatabaseHelper dbHelper = new DatabaseHelper(ExercisesActivity.this);
+                    dbHelper.deleteExercise(evh.exerciseName.getText().toString());
+                    finish();
+                    startActivity(getIntent());
+                }
+            });
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
 
     }
 
